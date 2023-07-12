0e2cd171a076e5a90b4b8e4d9383891559f689a9
ashokbudha102 <ashok.budha2015@gmail.com?>
2023-07-12 23:08:28 +0545

    category navbar and few changes

--- home/contextprocessor.py
+++ home/contextprocessor.py
@@ -2,14 +2,18 @@ from .models import Category, Author, Post, advertisement
 from datetime import date
 import nepali_datetime 
 
+
 # context processor to send context data to base.html
 
 def message_processor(request):
     development=Category.objects.filter(priority=2)
     posters=Post.objects.filter(approved=True).order_by('-timestamp')[:5]
     news=Category.objects.get(priority=8)
-    cappa=Category.objects.all().exclude(priority=2).order_by('priority')[0:3]
+    cappa=Category.objects.all().exclude(priority__in=[2,3]).order_by('priority')[0:2]
+    # cappa=Category.objects.all()
+    # cappa=cappa.objects.exclude(priority=[2,3])
     engsug=Category.objects.get(priority=6)
+    engedu=Category.objects.get(priority=3)
     dateeng=date.today()
     datenep=nepali_datetime.date.today()
     adtop=advertisement.objects.get(title='homepagetop')
@@ -18,7 +22,7 @@ def message_processor(request):
     d1=datenep.strftime("%K-%n-%D (%G)")
     if request.user.is_authenticated:
         current_user=Author.objects.get(user=request.user)
-        return {'aalu':cappa, 'dateeng':dateeng, 'datenep':d1,'current_user':current_user,'development':development,'engsug':engsug,'scroller':posters,'newer':news,'adtop':adtop,'newsside':newsside,}
+        return {'aalu':cappa, 'dateeng':dateeng, 'datenep':d1,'current_user':current_user,'development':development,'engsug':engsug,'scroller':posters,'newer':news,'adtop':adtop,'newsside':newsside,'engedu':engedu,}
     else:
-        return {'aalu':cappa, 'dateeng':dateeng, 'datenep':d1,'development':development,'engsug':engsug,'scroller':posters,'newer':news,'adtop':adtop,'newsside':newsside,}
+        return {'aalu':cappa, 'dateeng':dateeng, 'datenep':d1,'development':development,'engsug':engsug,'scroller':posters,'newer':news,'adtop':adtop,'newsside':newsside,'engedu':engedu,}
         
