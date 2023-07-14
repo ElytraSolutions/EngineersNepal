c72c1b4e3cfdecf65648cd7405aa5082f9c9c632
Manohar Dahal <manohardahal40@gmail.com>
2023-07-14 20:32:57 +0545

    admin panel milaaiyo

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-from .models import Post, Author, Category, advertisement, Vacancy, Videos, TenderDocuments, AppliedUsers, Epapers
+from .models import *
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','categories','breaking']
+    search_fields = ['title']
+    list_filter = ['categories','breaking']
+
+
+ 
+
+
+
 # Register your models here.
-admin.site.register(Post)
+
+admin.site.register(Post,PostAdmin)
 admin.site.register(Author)
 admin.site.register(Category)
 admin.site.register(advertisement)
@@ -13,3 +25,7 @@ admin.site.register(Epapers)
 
 
 
+
+
+
+   
