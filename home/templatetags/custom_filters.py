e93a647afaa24ee971d4830c9cb5e4c3f696935f
ashokbudha102 <ashok.budha2015@gmail.com?>
2023-08-21 12:00:30 +0545

    comment changed

--- home/templatetags/custom_filters.py
+++ home/templatetags/custom_filters.py
@@ -1,5 +1,5 @@
 from django import template
-
+from django.template.defaultfilters import stringfilter
 register = template.Library()
 
 @register.filter
@@ -15,4 +15,11 @@ def index(sequence, position):
 @register.simple_tag
 def set(val=None):
   return val
+
+@register.filter
+@stringfilter
+def upto(value, delimiter=None):
+   return value.split(delimiter)[0]
+upto.is_safe=True
+
 
