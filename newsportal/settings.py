6ef5a744b7d1798a130602139d9a65cd4aa08400
Saroj Prasad Mainali <98652711+VoidCU@users.noreply.github.com>
2023-08-25 22:11:43 +0545

    Update settings.py

--- newsportal/settings.py
+++ newsportal/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '0.0.0.0', 'engineersnepal.com','141.148.206.219','127.0.0.1',"www.engineersnepal.com"]
 
