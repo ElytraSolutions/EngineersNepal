39374a1fb71f6848035faee8ccd218778170e9f3
manohar322028 <manohardahal40@gmail.com>
2023-11-07 12:43:53 +0545

    database changed

--- newsportal/settings.py
+++ newsportal/settings.py
@@ -91,10 +91,26 @@ WSGI_APPLICATION = 'newsportal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': '',
+        'NAME': 'newsportal',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+
+
+
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
